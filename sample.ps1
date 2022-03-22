943108cf0a8d79629da670b1e7443b56e68c5885
gide02 <79551801+gide02@users.noreply.github.com>
2022-03-22 22:24:30 +0800

    Update sample.ps1

--- sample.ps1
+++ sample.ps1
@@ -1,1 +1,2 @@
-Copy-Item -Path "C:\Source\*" -Destination "C:\Target" -Recurse
+# Copy-Item -Path "C:\Source\*" -Destination "C:\Target" -Recurse
+$text = 'Hello World!' | Out-File $text -FilePath C:\Users\EB-Administrator\Desktop\textfromps1.txt
